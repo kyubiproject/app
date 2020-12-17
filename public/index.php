5a93ca1ef8b50814635bd9a3d1e872c7a06fa8f2
root <root@us1.citem.xyz>
2020-12-17 12:15:18 -0500

    server

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+if (! function_exists("array_key_first")) {
+	function array_key_first($array) {
+		return array_keys($array)[0] ?? null;
+	}
+}
+
 define('DIR_ROOT', dirname(__DIR__));
 
 require_once DIR_ROOT . '/vendor/autoload.php';
